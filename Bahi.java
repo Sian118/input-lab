ba6f3c12ba11e77d8e3d163f6bc19e55d10245a0
Sian118 <Sianahmedasif@gmail.com>
2025-02-25 16:03:06 +0500

    Add files via upload

---
+++ Bahi.java
@@ -0,0 +1,11 @@
+public class Bahi{
+ public static void main(String args[]){
+ Student s1=new Student("Ali","Islamabad",3.3);
+ s1.Display();
+Student s2=new Student("Ahmed","Islamabad",9.0);
+ s2.Display();
+
+}
+
+
+}
